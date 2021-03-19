c1b0d9a8179d2eb6015f0b4ea218e08541e92522
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-03-18 22:10:35 -0700

    Added prompt and confirm functions in sequence. No conditionals.

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // ⬇⬇⬇Overwrites whatever is in PW field with result of password function
+  // passwordText.value = password;
 
+  window.prompt("How many characters should your password be?")
+  // console.log();
+
+
+  window.confirm("Include capital letters?");
+
+  
+  window.confirm("Include lower case letters?") ;
+
+
+  window.confirm("Include numbers?");
+
+
+  window.confirm("Include special characters?");
+  
 }
 
 // Add event listener to generate button

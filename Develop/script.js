0a85ac2dfab945827ec466d1e4247afc25ecb125
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-03-19 22:50:55 -0700

    Added lower, number, special arrays. Created blank array to house user selections. added conditionals to add corresponding array to bigRandom array. Added some comments for reference. Removed all extra code/comments.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 // var password = generatePassword();
+
 function writePassword() {
   var passwordText = document.querySelector("#password");
   var passwordLength = 
@@ -11,19 +15,29 @@ function writePassword() {
   // ⬇⬇⬇Overwrites whatever is in PW field with result of password function
   // passwordText.value = password;
   
-  if (passwordLength < 8 || passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     window.alert("Please enter a valid number of characters.");
     return;
   }
   
   if (passwordLength >= 8 && passwordLength <= 128) {
 
-    // =====================================
-    //  Add this to an array, do for all types of chars
-    //  =====================================
+    // =================================
+    // Start collecting info for randomizer function
+    // =================================
     var allCaps = 
     ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var allLower = 
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var allSpecial = 
+    ["!", "@", "#", "$", "%", "&", "(", ")", "*", "+", ",", 
+    "-", ".", "/", ":", ";", "<", "=", ">", "?", "^","|", "~"];
+    var allNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    //  ⬇ Home array for combined user choices
+    var bigRandom = [];
+
     var rndmChar = Math.floor(Math.random() * allCaps.length);
     var rndmCap = allCaps[rndmChar];
     var includeCaps = window.confirm("Include capital letters?");
@@ -31,60 +45,43 @@ function writePassword() {
     var includeNumber = window.confirm("Include numbers?");
     var includeSpecials = window.confirm("Include special characters?");
     
-    console.log(includeCaps)
-    console.log(includeLower)
-    console.log(includeNumber)
-    console.log(includeSpecials)
+    // =======Testers============
+    // console.log(includeCaps);
+    // console.log(includeLower);
+    // console.log(includeNumber);
+    // console.log(includeSpecials);
+    // console.log(bigRandom);
+    // =======Testers============
+    
+    if (includeCaps) {
+      bigRandom = bigRandom.concat(allCaps);  
+    } 
+    
+    if (includeLower) {
+      bigRandom = bigRandom.concat(allLower);
+    }
+    
+    if (includeSpecials) {
+      bigRandom = bigRandom.concat(allSpecial);
+    }
 
-    // ===============================
-    // Add random index to password
-    // if password < passwordLength at end
-    // keep getting random chars from combined possibleChars array
-    // ===============================
+    if (includeNumber) {
+      bigRandom = bigRandom.concat(allNums);
+    }
 
+    console.log(bigRandom);
+    return
+        }
+      }
+      
+      
+      // ===============================
+      // Add random index to password
+      // if password < passwordLength at end
+      // keep getting random chars from combined possibleChars array
+      // ===============================
+    
 
-  //   if (includeCaps || !includeCaps) {
-      
-  //     console.log(includeLower);
-  //   } 
-    
-  //   if (includeLower || !includeLower) {
-  //     // window.confirm("Include numbers?");
-      
-  //     console.log(includeNumber);
-  //   }
-    
-  //   if (includeSpecials || !includeSpecials) {
-  //     // window.confirm("Include special characters?");
-      
-  //     console.log(includeSpecials);
-  //   }
-  }
-}
-    
-    
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
-    
     // Target Text field in HTML and output text inside
-    //    will need later
     // document.getElementById("password").innerHTML = Date();
     
-    
-    
-    //  JUST FOR REFERENCE:
-    //  Additional method for choosing random char in string
-    //  Alternative to Array method
-    // 
-    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    // function makeid() {
-    //   var text = "";
-    //   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    //   for (var i = 0; i < 5; i++)
-    //     text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    //   return text;
-    // }
-
-    // console.log(makeid());
